528959a9ed8deb9060d2ae1253c5f40eea8fc6c4
anshb49 <anshbhalla1@gmail.com>
2020-06-12 14:26:45 -0700

    Created second viewcontroller.swift

---
+++ ExpenseApp/AddExpenseViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddExpenseViewController.swift
+//  ExpenseApp
+//
+//  Created by Ansh Bhalla on 6/12/20.
+//  Copyright © 2020 Ansh Bhalla. All rights reserved.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+    
+    @IBOutlet weak var ConfirmAdd: UIButton!
+    
+    @IBOutlet weak var LeavePage: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+        
+        ConfirmAdd.layer.cornerRadius = 20
+        ConfirmAdd.clipsToBounds = true
+        
+        LeavePage.layer.cornerRadius = 20
+        LeavePage.clipsToBounds = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
