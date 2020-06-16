c1e2c27f848040f7cbafe5e92f373dd7e0e95ed5
ptelag2 <ptelag2@illinois.edu>
2020-06-16 17:06:05 -0500

    Added expense class

---
+++ ExpenseApp/expense.swift
@@ -0,0 +1,16 @@
+//
+//  expense.swift
+//  ExpenseApp
+//
+//  Created by Praval Telagi on 6/16/20.
+//  Copyright © 2020 Ansh Bhalla. All rights reserved.
+//
+
+import Foundation
+
+class Expense {
+    var name = ""
+    var date = ""
+    var time = ""
+    var amount = 0.0
+}
