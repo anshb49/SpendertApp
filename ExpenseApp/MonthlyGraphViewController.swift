512e84e4fd09ba87fa004824419a0b5fabbe6705
ptelag2 <ptelag2@illinois.edu>
2020-06-29 12:29:28 -0500

    Bar Graph Working with Month Labels

--- ExpenseApp/MonthlyGraphViewController.swift
+++ ExpenseApp/MonthlyGraphViewController.swift
@@ -16,21 +16,60 @@ class MonthlyGraphViewController: UIViewController, ChartViewDelegate {
 
     lazy var lineChartView: LineChartView = {
         let chartView = LineChartView()
-        chartView.backgroundColor = .systemBlue
+        chartView.backgroundColor = .systemGray
+        chartView.rightAxis.enabled = false
+        
+        let yAxis = chartView.leftAxis
+        
+        yAxis.labelFont = .boldSystemFont(ofSize: 12)
+        yAxis.setLabelCount(6, force: false)
+        yAxis.labelTextColor = .white
+        yAxis.axisLineColor = .white
+        yAxis.labelPosition = .insideChart
+        
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+        chartView.xAxis.setLabelCount(6, force: false)
+        chartView.xAxis.axisLineColor = .white
+        chartView.xAxis.labelTextColor = .white
+        
+        chartView.animate(xAxisDuration: 0.5)
+        
         return chartView
     }()
     
+    lazy var barChartView: BarChartView = {
+        let barView = BarChartView()
+        barView.backgroundColor = .systemGray
+        barView.xAxis.labelPosition = .bottom
+        barView.xAxis.valueFormatter = IndexAxisValueFormatter(values: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"])
+        barView.xAxis.granularity = 1
+        barView.xAxis.setLabelCount(12, force: false)
+        barView.rightAxis.enabled = false
+        barView.leftAxis.enabled = false
+        //barView.animate(yAxisDuration: 1.25)
+        return barView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        view.addSubview(lineChartView)
+        /*view.addSubview(lineChartView)
         
         lineChartView.centerInSuperview()
         lineChartView.width(to: view)
-        lineChartView.heightToWidth(of: view)
+        lineChartView.heightToWidth(of: view)*/
+        
+        view.addSubview(barChartView)
+        barChartView.centerInSuperview()
+        barChartView.width(to: view)
+        barChartView.heightToWidth(of: view)
+        //barChartView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        barChartView.center = view.center
+        //view.addSubview(barChartView)
+        
         
         setData()
         
@@ -41,10 +80,29 @@ class MonthlyGraphViewController: UIViewController, ChartViewDelegate {
     }
     
     func setData() {
+        let set1 = BarChartDataSet(entries: monthlyExpenseValues, label: "Monthly Expenses")
+        set1.colors = ChartColorTemplates.joyful()
+        let data = BarChartData(dataSet:set1)
+        barChartView.data = data
+        
+    }
+    
+    /*func setData() {
         let set1 = LineChartDataSet(entries: yValues, label: "subscribers")
+        
+        set1.drawCirclesEnabled = false
+        set1.mode = .cubicBezier
+        set1.lineWidth = 3
+        set1.setColor(.white)
+        set1.drawHorizontalHighlightIndicatorEnabled = false
+        set1.highlightColor = .white
+        
         let data = LineChartData(dataSet: set1)
         lineChartView.data = data
-    }
+        data.setDrawValues(false)
+        
+        
+    }*/
     
     let yValues: [ChartDataEntry] = [
         ChartDataEntry(x: 0.0, y: 10.0),
@@ -58,6 +116,27 @@ class MonthlyGraphViewController: UIViewController, ChartViewDelegate {
         ChartDataEntry(x: 8.0, y: 8.0),
         ChartDataEntry(x: 9.0, y: 12.0),
         ChartDataEntry(x: 10.0, y: 13.0)
+    ]
+    
+    let monthValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 6/20, y: 34),
+        ChartDataEntry(x: 7/20, y: 234)
+    ]
+    
+    let monthlyExpenseValues: [BarChartDataEntry] = [
+        /*BarChartDataEntry(x: 6, y: 211.38),
+        BarChartDataEntry(x: 7, y: 234)*/
+        
+        BarChartDataEntry(x: 0.0, y: 0.0),
+        BarChartDataEntry(x: 1.0, y: 1.0),
+        BarChartDataEntry(x: 2.0, y: 2.0),
+        BarChartDataEntry(x: 3.0, y: 3.0),
+        BarChartDataEntry(x: 4.0, y: 4.0),
+        BarChartDataEntry(x: 5.0, y: 5.0),
+        BarChartDataEntry(x: 6.0, y: 6.0),
+        BarChartDataEntry(x: 7.0, y: 7.0),
+        BarChartDataEntry(x: 8.0, y: 8.0),
+        BarChartDataEntry(x: 9.0, y: 9.0)
     ]
     
     
