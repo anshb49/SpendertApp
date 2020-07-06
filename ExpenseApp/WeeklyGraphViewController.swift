f21695d0b77386423e53bda549ce6e24a27f9559
ptelag2 <ptelag2@illinois.edu>
2020-07-06 17:20:57 -0500

    WeeklyGraph Test files setup

---
+++ ExpenseApp/WeeklyGraphViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeeklyGraphViewController.swift
+//  ExpenseApp
+//
+//  Created by Praval Telagi on 7/6/20.
+//  Copyright © 2020 Ansh Bhalla. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyGraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
