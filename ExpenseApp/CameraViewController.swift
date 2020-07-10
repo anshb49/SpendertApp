bd0ac7acb31ab04cc549ddd3723e82a7574a2651
anshb49 <anshbhalla1@gmail.com>
2020-07-10 16:44:02 -0700

    basic camera test

---
+++ ExpenseApp/CameraViewController.swift
@@ -0,0 +1,120 @@
+//
+//  CameraViewController.swift
+//  ExpenseApp
+//
+//  Created by Ansh Bhalla on 7/10/20.
+//  Copyright © 2020 Ansh Bhalla. All rights reserved.
+//
+
+import UIKit
+import Vision
+import VisionKit
+
+class CameraViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var textRecognitionRequest = VNRecognizeTextRequest(completionHandler: nil)
+    private let textRecognitionWorkQueue = DispatchQueue(label: "MyVisionScannerQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVision()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupVision() {
+        textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            var detectedText = ""
+            for observation in observations {
+                guard let topCandidate = observation.topCandidates(1).first else { return }
+                print("text \(topCandidate.string) has confidence \(topCandidate.confidence)")
+    
+                detectedText += topCandidate.string
+                detectedText += "\n"
+                
+            
+            }
+            
+            DispatchQueue.main.async {
+                self.textView.text = detectedText
+                self.textView.flashScrollIndicators()
+
+            }
+        }
+
+        textRecognitionRequest.recognitionLevel = .accurate
+    }
+    
+    private func processImage(_ image: UIImage) {
+        imageView.image = image
+        recognizeTextInImage(image)
+    }
+    
+    private func recognizeTextInImage(_ image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
+        
+        textView.text = ""
+        textRecognitionWorkQueue.async {
+            let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try requestHandler.perform([self.textRecognitionRequest])
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        guard scan.pageCount >= 1 else {
+            controller.dismiss(animated: true)
+            return
+        }
+        
+        let originalImage = scan.imageOfPage(at: 0)
+        let newImage = compressedImage(originalImage)
+        controller.dismiss(animated: true)
+        
+        processImage(newImage)
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        print(error)
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        controller.dismiss(animated: true)
+    }
+
+    func compressedImage(_ originalImage: UIImage) -> UIImage {
+        guard let imageData = originalImage.jpegData(compressionQuality: 1),
+            let reloadedImage = UIImage(data: imageData) else {
+                return originalImage
+        }
+        return reloadedImage
+    }
+    
+
+    @IBAction func OpenCamera(_ sender: Any) {
+        let scannerViewController = VNDocumentCameraViewController()
+        scannerViewController.delegate = self
+        present(scannerViewController, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
